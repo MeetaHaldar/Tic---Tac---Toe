cb512d82d30cb229bf7d2c5b15d3bc339b4aab9d
meeta haldar <meetahaldar1001@gmail.com>
2023-11-27 13:54:18 +0530

    dk

---
+++ index.js
@@ -0,0 +1,20 @@
+let music=new Audio("music.mp3");
+let ting=new Audio("ting.mp3");
+let GameOver=new Audio("gameover.mp3");
+
+let myTurn ="X";
+// function to change turn 
+const changeTurn=()=>
+{
+    return myTurn==="X"?"0":"X";
+}
+
+
+// function to check win
+const checkWin=()=>
+{
+
+}
+
+
+// game logic
